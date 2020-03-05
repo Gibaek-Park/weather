1f6d27e30c24d52ec6a3ed427582951b7c7da7b8
gibaek Park <yaqqww@nate.com>
2020-03-05 23:35:44 +0900

    add: webpack, babel 설정

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+require("babel-polyfill");
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/index.js"],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/dist")
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    })
+  ]
+};
