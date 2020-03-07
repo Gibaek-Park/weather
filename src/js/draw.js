8130fc4b8b98a5b2a857cb291da376699173ed9a
gibaek Park <yaqqww@nate.com>
2020-03-07 21:29:10 +0900

    update: 현재 날씨 관련 info 업데이트

--- src/js/draw.js
+++ src/js/draw.js
@@ -1,24 +1,53 @@
-const wrap = document.querySelector('#wrap');
+import { getDateInfo } from './util/util'
 
 const draw = {
   currentWeatherContent(res) {
-    const { data, sys } = res;
-    const imgWrap = document.querySelector('#currentWeatherWrap');
+    const { main: { temp, feels_like, temp_min, temp_max },
+      weather, wind, sys, name, cloudes, dt } = res.data;
+    const currentWeather = document.querySelector('#currentWeather');
 
-    const currentWeahter = document.createElement('div');
-    currentWeahter.classList.add('currentWeather');
+    const currentTime = document.createElement('p');
+    const date = new Date(dt * 1000);
+    const { dayOfWeek, year, month, day, hour } = getDateInfo(date);
+    currentTime.textContent = `(${dayOfWeek}요일) ${hour}:00, ${year}년 ${month}월 ${day}일`
 
-    const headerText = document.createElement('h3');
-    headerText.textContent = `Current Weather in ${data.name}, ${sys.country}`;
+    const headerText = document.createElement('h2');
+    headerText.textContent = `Current Weather in ${name}, ${sys.country}`;
 
     const img = document.createElement('img');
-    const icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    const icon = "http://openweathermap.org/img/w/" + weather[0].icon + ".png";
     img.setAttribute('src', icon);
 
-    imgWrap.children[0] && imgWrap.removeChild(imgWrap.children[0]);
-    currentWeahter.appendChild(headerText);
-    currentWeahter.appendChild(img);
-    imgWrap.appendChild(currentWeahter);
+    const infoWeather = document.createElement('div');
+    infoWeather.id = 'currentWeatherInfo';
+
+    const tempWrap = document.createElement('p');
+    tempWrap.innerHTML = `${Math.floor(temp)}°C`;
+
+    const tempMinWrap = document.createElement('span');
+    tempMinWrap.innerHTML = `${Math.floor(temp_min)}°C`;
+
+    const tempMaxWrap = document.createElement('span');
+    tempMaxWrap.innerHTML = `${Math.floor(temp_max)}°C`;
+    
+    const feelsLikeWrap = document.createElement('span');
+    feelsLikeWrap.innerHTML = `체감온도: ${Math.floor(feels_like)}°C`;
+
+    infoWeather.appendChild(tempWrap);
+    infoWeather.appendChild(tempMinWrap);
+    infoWeather.appendChild(tempMaxWrap);
+    infoWeather.appendChild(feelsLikeWrap);
+
+    if(currentWeather.children.length !== 0) {
+      while (currentWeather.firstChild) {
+        currentWeather.removeChild(currentWeather.firstChild);
+      }
+    }
+
+    currentWeather.appendChild(currentTime);
+    currentWeather.appendChild(headerText);
+    currentWeather.appendChild(img);
+    currentWeather.appendChild(infoWeather);
   },
 
   forecastWeatherContent(res) {
